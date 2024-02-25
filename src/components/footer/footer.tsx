7c921bc3ad249be3e214c8eb6a143140bc61eb42
Eder <eder.achaica@gmail.com>
2024-02-25 15:55:54 -0500

    fix copyright year

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,6 +3,7 @@ import LogoMobile from "../logo-mobile";
 import "./footer.scss";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="layout__footer">
       <div className="container layout__footer__content">
@@ -11,7 +12,7 @@ function Footer() {
 
         <hr className="layout__footer__content__divider" />
         <span className="layout__footer__content__copyright">
-          © 2023 RIMAC Seguros y Reaseguros.
+          {`© ${currentYear} RIMAC Seguros y Reaseguros.`}
         </span>
       </div>
     </footer>
